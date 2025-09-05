87bf844292c0e7d079e42c83fe013edff596b07c
Bofi1 <angelborque03github@gmail.com>
2025-09-04 21:09:52 -0700

    desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        desktop: "1000px",
+      },
       fontFamily: {
         OpenSans: ["'Open Sans'", "sans-serif"],
       },
